68dede0868deb4d17cae09d587d2e80b23561640
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-06-20 23:40:50 +0200

    wipwip

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./css/normalize.css";
 import "./css/styles.css";
-import drumPads from "./data/drums-808.json";
-import Pads from "./components/pad";
+import Pad from "./components/pad";
 
 function App() {
-  const [pad, setPads] = useState({});
-
-  useEffect(() => {
-    setPads(drumPads);
-  }, []);
-
-  console.log(pad);
-
   return (
     <div className="app">
       <header className="app-header">React Drums</header>
       <section className="app-panel">
-        <Pads />
+        <Pad />
       </section>
     </div>
   );
