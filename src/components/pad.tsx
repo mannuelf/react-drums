c46ec141dea7bd3ed691ac0aec062bea88db94dd
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-06-21 23:12:51 +0200

    wip

---
+++ src/components/pad.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import _ from "lodash";
+import { getDrums } from "../data/drums";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: #444;
+  border-radius: 3px;
+  color: yellow;
+  margin: 0 0.5em;
+  padding: 1rem;
+  border: none;
+  height: 100%;
+  width: 100%;
+  cursor: pointer;
+  box-shadow: -2px 2px 2px rgba(0, 0, 0, 0.4);
+`;
+
+class Pad extends Component {
+  state = {
+    kit: {
+      id: 1,
+      name: "808",
+      sounds: {
+        boom: "../sounds/808/boom.wav",
+        clap: "../sounds/808/clap.wav",
+        hihat: "../sounds/808/hihat.wav",
+        kick: "../sounds/808/kick.wav",
+        openhat: "../sounds/808/ride.wav",
+        snare: "../sounds/808/snare.wav",
+        tink: "../sounds/808/tom.wav"
+      }
+    }
+  };
+
+  componentDidMount() {}
+
+  render() {
+    const { kit } = this.state;
+    const sounds = kit.sounds;
+    const audioElements = Object.values(sounds).map((sound, index) => (
+      <audio src={sound} data-key={index} key={index}></audio>
+    ));
+    return audioElements;
+  }
+}
+
+export default Pad;
