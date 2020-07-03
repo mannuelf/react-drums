5b60d763bd7bfa1add303860b7f9fd2057c2c09c
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-07-03 21:55:44 +0200

    tidy up

--- src/components/drumpads.tsx
+++ src/components/drumpads.tsx
@@ -1,6 +1,5 @@
-import React, { Fragment, Component } from "react";
+import React, { Component } from "react";
 import Pad from "./pad";
-import _ from "lodash";
 
 // https://github.com/typescript-cheatsheets/react-typescript-cheatsheet/issues/57
 type Props = {
@@ -19,21 +18,15 @@ class DrumPad extends Component<Props, State> {
     };
   }
 
-  componentDidMount() {
-    console.log("Pad mounted", this.props);
-  }
-
   render() {
     const { kit } = this.props;
     const kitArray = Object.entries(kit);
     const display = kitArray[0][1];
     const { sounds } = display;
     return (
-      <Fragment>
-        {console.log("sounds", display)}
-        <h1 className="title">808 Drum</h1>
-        <Pad id={1} name="808" url="./sounds/" sounds={sounds} keyboard="q" />
-      </Fragment>
+      <>
+        <Pad sounds={sounds} key={""} />
+      </>
     );
   }
 }
