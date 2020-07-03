22d616ded6ec5f837454fd14aaa2fb63950c6c9c
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-07-03 16:41:29 +0200

    KISS, just use plain ole css for now

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "./css/normalize.css";
+import "./css/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
