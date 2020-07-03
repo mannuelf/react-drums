d3d37ced1ed4b6d8dafddc5365fe91550453ab83
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-07-03 21:53:27 +0200

    add id for key mapw

--- src/data/drums.ts
+++ src/data/drums.ts
@@ -12,54 +12,63 @@ export const drums: Drum[] = [
     name: "808",
     sounds: {
       boom: {
+        id: 1,
         name: "boom",
         clip: "./sounds/808/boom.wav",
         key: 81,
         letter: "q"
       },
       clap: {
+        id: 2,
         name: "clap",
         clip: "./sounds/808/clap.wav",
         key: 87,
         letter: "w"
       },
       hihat: {
+        id: 3,
         name: "hihat",
         clip: "./sounds/808/hihat.wav",
         key: 69,
         letter: "e"
       },
       kick: {
+        id: 4,
         name: "kick",
         clip: "./sounds/808/kick.wav",
         key: 82,
         letter: "r"
       },
       openhat: {
+        id: 5,
         name: "openhat",
         clip: "./sounds/808/openhat.wav",
         key: 84,
         letter: "t"
       },
       ride: {
+        id: 6,
         name: "ride",
         clip: "./sounds/808/ride.wav",
         key: 89,
         letter: "y"
       },
       snare: {
+        id: 7,
         name: "snare",
         clip: "./sounds/808/snare.wav",
         key: 85,
         letter: "u"
       },
       tink: {
+        id: 8,
         name: "tink",
         clip: "./sounds/808/tink.wav",
         key: 73,
         letter: "i"
       },
       tom: {
+        id: 9,
         name: "tom",
         clip: "./sounds/808/tom.wav",
         key: 79,
