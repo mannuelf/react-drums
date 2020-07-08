b23d1cc8abe5a1c9a37750a130c8371992e82aa4
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-07-09 00:24:32 +0200

    add header component and d.ts

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import SVGLogo from "../img/maschinelogo.svg";
+
+const Header = () => {
+  return (
+    <>
+      <header className="app-header">
+        <div className="column">
+          <img src={SVGLogo} className="logo" alt="MASCHINE MIKRO" />
+        </div>
+        <div className="column">
+          <div className="app-header-lcd">
+            <h1>
+              React
+              <div>808 Drum</div>
+            </h1>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
