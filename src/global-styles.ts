542997f478ce9a062adb433ef20dcf89f4c785d5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-10-08 22:37:24 +0200

    deps udpates and style update

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -7,6 +7,7 @@ const GlobalStyle = createGlobalStyle`
 }
 
 body {
+  overflow: hidden;
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
