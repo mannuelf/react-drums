4a180310db0ff8c111411b57cdeba45126463e9a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-03-04 23:17:27 +0100

    wip: creating server

--- src/utils/getDrums.ts
+++
@@ -1,9 +0,0 @@
-import { drums } from "../data/drums";
-
-export function getDrums(): Kit[] {
-  return drums;
-}
-
-export function getDrumKitByName(kitName: string): Kit | undefined {
-  return drums.find((kit) => kit.name === kitName);
-}
