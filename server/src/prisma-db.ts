1aceddee642ba2ed07492451a2cfe9bf9bedbce6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-15 13:15:11 +0200

    connect to db, ability to CRUD using prisma

--- server/src/prisma-db.ts
+++ server/src/prisma-db.ts
@@ -3,6 +3,15 @@ import { PrismaClient } from '@prisma/client';
 const prisma: PrismaClient = new PrismaClient();
 
 async function main() {
+  const newUser = await prisma.user.create({
+    data: {
+      firstName: 'Jack',
+      lastName: 'Black',
+      email: 'jackblack@email.com',
+      password: 'password',
+      createdAt: new Date(),
+    },
+  });
   const allUsers = await prisma.user.findMany();
   console.log(allUsers);
 }
