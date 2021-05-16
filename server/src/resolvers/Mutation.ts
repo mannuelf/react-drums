a65cf2eefb5f4033ff7d9e0ab79dcca9a6f0e6cf
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-16 08:30:32 +0200

    server: converted to function expression for better workflow can import them with wildcard in the server initialise file

--- server/src/resolvers/Mutation.ts
+++ server/src/resolvers/Mutation.ts
@@ -1,20 +1,24 @@
-import jwt from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import { APP_SECRET } from '../constants';
 
-export async function signup(parent: any, args: any, context: any) {
+const { sign } = jwt;
+
+export const signup = async (parent, args, context): Promise<any> => {
+  console.log('Mutation: signup', args);
   const password = await bcrypt.hash(args.password, 10);
 
   const user = await context.prisma.user.create({
     data: { ...args, password },
   });
-
-  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+  console.log('>>', user);
+  const token = sign({ userId: user.id }, APP_SECRET);
 
   return { token, user };
-}
+};
 
-export async function login(parent: any, args: any, context, info) {
+export const login = async (parent, args, context, info): Promise<any> => {
+  console.log('Mutation: login', args);
   const user = await context.prisma.user.findUnique({
     where: { email: args.email },
   });
@@ -28,7 +32,20 @@ export async function login(parent: any, args: any, context, info) {
     throw new Error('Invalid password');
   }
 
-  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+  const token = sign({ userId: user.id }, APP_SECRET);
 
   return { token, user };
-}
+};
+
+export const user = async (parent, args, context, info): Promise<any> => {
+  console.log('Mutation: user', args);
+  const { userId } = context;
+  return await context.prisma.user.create({
+    data: {
+      firstName: args.firstName,
+      lastName: args.lastName,
+      email: args.email,
+      password: args.password,
+    },
+  });
+};
