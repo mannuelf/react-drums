6542685b7e931f0fc8e705b9912f962cafcceb67
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-16 08:34:01 +0200

    server: integrated resolver, Query and Mutations with server. Import all function expressions into server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,60 +1,23 @@
-import { Request } from 'express';
-import { ApolloServer } from 'apollo-server';
-import { PrismaClient } from '@prisma/client';
 import fs from 'fs';
 import path from 'path';
+import { ApolloServer } from 'apollo-server';
+import { PrismaClient } from '@prisma/client';
+
+import * as Query from './resolvers/Query';
+import * as Mutation from './resolvers/Mutation';
 import { getUserId } from './utils';
 
 const prisma: PrismaClient = new PrismaClient();
 
-const users = [
-  {
-    id: 1,
-    firstName: 'Mannuel',
-    lastName: 'Ferreira',
-    email: 'mannuel@email.com',
-    password: 'password',
-    createdAt: new Date(),
-  },
-  {
-    id: 2,
-    firstName: 'Jack',
-    lastName: 'Ferreira',
-    email: 'jack@email.com',
-    password: 'password',
-    createdAt: new Date(),
-  },
-];
-
-let idCount = users.length;
-
 const resolvers = {
-  Query: {
-    info: () => `API for all the sounds`,
-    users: async (parent: any, args: any, context: any) => {
-      return context.prisma.user.findMany();
-    },
-  },
-  Mutation: {
-    user: (parent: any, args: any) => {
-      const user = {
-        id: idCount++,
-        firstName: args.firstName,
-        lastName: args.lastName,
-        email: args.email,
-        password: args.password,
-        createdAt: new Date(),
-      };
-      users.push(user);
-      return user;
-    },
-  },
+  Query,
+  Mutation,
 };
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
-  context: ({ req: Request }): Promise<any> => {
+  context: ({ req }) => {
     return {
       ...req,
       prisma,
@@ -66,7 +29,7 @@ const server = new ApolloServer({
 server
   .listen()
   .then(({ url }) => {
-    console.log(`Server is running on ${url}`);
+    console.log(`🏗  Server is running on ${url}`);
   })
   .catch((error) => {
     console.log(error);
