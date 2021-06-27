baeb1396ca93be085abcb7866f740c7bc51abb3d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-27 13:46:04 +0200

    build: update header to show version number, why not. its fun.

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,15 +1,17 @@
+import version from '../../package.json';
+import { LOGO } from '../constants';
+
 const Header = () => {
-  const logo = `https://res.cloudinary.com/mannuel/image/upload/v1594123808/images/maschinelogo.svg`;
   return (
     <>
       <header className='app-header'>
         <div className='column'>
-          <img src={logo} className='logo' alt='MASCHINE Micro' />
+          <img src={LOGO} className='logo' alt='MASCHINE Micro' />
         </div>
         <div className='column'>
           <div className='app-header-lcd'>
             <h1>
-              MACHINE Micro v.1
+              Micro v.{version?.version}
               <div>808 Drums</div>
             </h1>
           </div>
--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,1 +1,2 @@
 export const AUTH_JWT = 'jwt';
+export const LOGO = `https://res.cloudinary.com/mannuel/image/upload/v1594123808/images/maschinelogo.svg`;
