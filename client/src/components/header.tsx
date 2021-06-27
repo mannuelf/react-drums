39d6cac819f8cb0681ac3eb4121f922cb0456b4f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-27 13:21:09 +0200

    styling: update app name

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,14 +2,14 @@ const Header = () => {
   const logo = `https://res.cloudinary.com/mannuel/image/upload/v1594123808/images/maschinelogo.svg`;
   return (
     <>
-      <header className="app-header">
-        <div className="column">
-          <img src={logo} className="logo" alt="MASCHINE MIKRO" />
+      <header className='app-header'>
+        <div className='column'>
+          <img src={logo} className='logo' alt='MASCHINE Micro' />
         </div>
-        <div className="column">
-          <div className="app-header-lcd">
+        <div className='column'>
+          <div className='app-header-lcd'>
             <h1>
-              React
+              MACHINE Micro v.1
               <div>808 Drums</div>
             </h1>
           </div>
