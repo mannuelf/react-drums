6654ed21f9bd530919dcb136d4e69da899db82a1
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-10-10 08:52:54 +0200

    styling

--- client/src/components/common/button.tsx
+++ client/src/components/common/button.tsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
-export const Button = styled.button`
-  background: #444;
-  border-radius: 3px;
-  padding: 1rem;
-  border: none;
-  height: 68px;
-  width: 100%;
-  cursor: pointer;
-  box-shadow: -2px 2px 2px rgba(0, 0, 0, 0.4);
-`;
+export const Button = styled('button')({
+  background: '#444',
+  borderRadius: '3px',
+  padding: '1rem',
+  border: 'none',
+  height: '68px',
+  width: '100%',
+  cursor: 'pointer',
+  boxShadow: '-2px 2px 2px rgba(0, 0, 0, 0.4)',
+});
