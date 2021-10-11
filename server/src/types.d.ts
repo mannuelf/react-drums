c35f6a66e8cf7921a98d2487ecb64671bf3f835f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-10-11 22:15:07 +0200

    export

--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -1,18 +1,18 @@
-interface IUser {
+export interface IUser {
   id: number;
   firstName: string;
   lastName: string;
   email: string;
-  password?: string;
+  password: string;
   createdAt: string;
   updatedAt: string;
 }
 
-interface IUsers {
+export interface IUsers {
   users: [IUser];
 }
 
-interface AuthPayload {
+export interface AuthPayload {
   token: string;
   user: [IUser];
 }
