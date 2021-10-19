1d519026129de1ac513cc2fe0129fb70f3951ebc
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-10-19 22:05:51 +0200

    unit test to return

--- frontend/jest.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
--- frontend/src/features/machine/Machine.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from './Machine';
-
-test('renders without issues', () => {
-  const { getByText } = render(<App />);
-  const headElement = getByText(/Micro v0.0.3/i);
-  expect(headElement).toBeInTheDocument();
-});
