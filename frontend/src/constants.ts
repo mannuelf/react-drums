822a5d366fca443f07a7c6cfd6164177ce4f37df
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-10-23 07:53:39 +0200

    frontend: add new logo

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -3,4 +3,8 @@ import.meta.hot;
 export const AUTH_JWT = 'AUTH';
 export const AUTH_TOKEN = 'akljshdksahdkhd';
 export const API_URL = 'http://localhost:4000';
-export const LOGO = `https://res.cloudinary.com/mannuel/image/upload/v1594123808/images/maschinelogo.svg`;
+export const LOGO = {
+  light: 'https://res.cloudinary.com/mannuel/image/upload/v1634885670/images/logo-light.svg'
+  ,
+  dark: 'https://res.cloudinary.com/mannuel/image/upload/v1634885670/images/logo-dark.svg'
+}
--- frontend/src/features/machine/MachineHeader.tsx
+++ frontend/src/features/machine/MachineHeader.tsx
@@ -6,7 +6,11 @@ const MachineHeader: React.FC = (): JSX.Element => {
     <>
       <header className='app-header'>
         <div className='column'>
-          <img src={LOGO} className='logo' alt='Machine Micro' />
+          <img
+            src={LOGO.light}
+            className='logo'
+            alt='Machine Micro'
+          />
         </div>
         <div className='column'>
           <div className='app-header-lcd'>
