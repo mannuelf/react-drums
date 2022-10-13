0b10d282deacf0be6910de4952d7681252b8cdfc
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-10-13 07:41:02 +0200

    wip

--- client/esbuild.config.production.js
+++ client/esbuild.config.production.js
@@ -2,16 +2,17 @@ import * as esbuild from 'esbuild';
 
 esbuild
   .build({
-    entryPoints: ['./src/index.tsx'],
     bundle: true,
+    entryPoints: ['./src/index.tsx'],
+    logLevel: 'verbose',
     minify: true,
-    platform: 'node',
-    sourcemap: true,
     outfile: './dist/bundle.js',
+    platform: 'node',
     plugins: [],
+    sourcemap: true,
   })
   .catch((error) => {
-    console.log('🔥 error', error);
+    console.error('🔥 error', error);
     process.exit(1);
   })
   .finally(() => {
