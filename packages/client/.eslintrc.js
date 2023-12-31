7b84c91d52be1dbe5e7f05dbebf80acea6dc5c2e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-12-31 15:56:09 +0100

    chore(npm): update npm scripts clean up unrequired packages

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   plugins: ['jest-dom'],
   extends: [
+    'plugin:react-hooks/recommended',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
