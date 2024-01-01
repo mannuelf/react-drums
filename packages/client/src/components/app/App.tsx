abd338d33fd6d0a2c5d0c4bad01bad5aeb47750f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2024-01-01 11:02:54 +0100

    chore(tidy): remove unrequired

--- packages/client/src/components/app/App.tsx
+++ packages/client/src/components/app/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useQuery } from 'urql';
-import LoginForm from '../../features/login/LoginForm';
 import Machine from '../../features/machine/Machine';
 import { GetUsersDocument } from '../../graphql/generated';
 import Footer from '../footer/Footer';
@@ -13,7 +12,6 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Machine />} />
-        <Route path='/login' element={<LoginForm />} />
       </Routes>
       <Footer />
     </BrowserRouter>
